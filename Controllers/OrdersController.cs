ffc33c0716f37dc16bd22d474f4da852eb862388
AntiD2ta <mtpotro41@gmail.com>
2021-03-13 12:20:44 -0500

    Add PATCH and DELETE api/orders/ endpoint
    Allow the administrators to change the state of an order and to delete a order only if is not confirmed

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OnlineStore.Api.Data;
@@ -35,15 +36,69 @@ namespace OnlineStore.Api.Controllers
             return orden;
         }
 
+        // [HttpPatch("{id}")]
+        // public async Task<IActionResult> ChangeState(int id,
+        //     [FromBody] JsonPatchDocument<Orden> patchOrder)
+        // {
+        //     if (patchOrder != null)
+        //     {
+        //         var orden = await _context.Ordenes.FindAsync(id);
+
+        //         if (orden == null) return NotFound();
+
+        //         patchOrder.ApplyTo(orden, ModelState);
+
+        //         if (!ModelState.IsValid)
+        //         {
+        //             return BadRequest(ModelState);
+        //         }
+
+        //         _context.Entry(orden).State = EntityState.Modified;
+        //         await _context.SaveChangesAsync();
+
+        //         return new ObjectResult(orden);
+        //     }
+        //     else
+        //     {
+        //         return BadRequest(ModelState);
+        //     }
+        // }
+
         [HttpPatch("{id}")]
-        [Authorize]
+        [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> ChangeState(int id, EstadoOrden estado)
+        {
+            if (estado != EstadoOrden.none)
+            {
+                var orden = await _context.Ordenes.FindAsync(id);
+
+                if (orden == null) return NotFound();
+
+                _context.Entry(orden).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+
+                return new ObjectResult(orden);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Delete(int id)
         {
             var orden = await _context.Ordenes.FindAsync(id);
 
             if (orden == null) return NotFound();
 
-            
+            if (orden.Estado == EstadoOrden.confirmed) return BadRequest();
+
+            _context.Ordenes.Remove(orden);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
