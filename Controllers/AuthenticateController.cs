4b1875a1da3e4908d1a06065b1ef1a3c05537d2f
AntiD2ta <mtpotro41@gmail.com>
2021-03-13 12:17:42 -0500

    Update Authenticate Controller to add claim Role to JWT token

--- Controllers/AuthenticateController.cs
+++ Controllers/AuthenticateController.cs
@@ -15,24 +15,27 @@ namespace OnlineStore.Api.Controllers
     [Route("api/[controller]")]
     public class AuthenticateController : Controller
     {
-        private UserManager<Usuario> userManager;
+        private UserManager<Usuario> _userManager;
 
         public AuthenticateController(UserManager<Usuario> userManager)
         {
-            this.userManager = userManager;
+            _userManager = userManager;
         }
+
 
         [HttpPost]
         [Route("login")]
         public async Task<IActionResult> Login([FromBody] LoginModel model)
         {
-            var user = await userManager.FindByNameAsync(model.Username);
-            if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
+            var user = await _userManager.FindByNameAsync(model.Username);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
             {
                 var authClaims = new[]
                 {
                     new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
-                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
                 };
                 var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("7S79jvOkEdwoRqHx"));
                 var token = new JwtSecurityToken(
