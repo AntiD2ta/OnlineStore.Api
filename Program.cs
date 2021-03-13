e9cd85be36ede76e6ebe7b8296d62ce1985cf4f7
AntiD2ta <mtpotro41@gmail.com>
2021-03-13 12:25:18 -0500

    Update Program.cs to call DB seeder at the start of the program

--- Program.cs
+++ Program.cs
@@ -2,10 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoPets.Api.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OnlineStore.Api.Data;
+using OnlineStore.Api.Data.Models;
 
 namespace OnlineStore.Api
 {
@@ -13,7 +18,7 @@ namespace OnlineStore.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().SeedDatabase().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +27,21 @@ namespace OnlineStore.Api
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+    }
+
+    public static class IHostExtensions
+    {
+        public static IHost SeedDatabase(this IHost host)
+        {
+            var scopeFactory = host.Services.GetRequiredService<IServiceScopeFactory>();
+            using var scope = scopeFactory.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<OnlineStoreContext>();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<Usuario>>();
+
+            if (context.Database.EnsureCreated())
+                SeedData.Initialize(context, userManager);
+
+            return host;
+        }
     }
 }
